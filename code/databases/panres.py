8503d737ec961200c4bf9bf9b4aad20dc073b0e2
hmmartiny <hanmar@food.dtu.dk>
2025-03-25 13:10:05 +0100

    Documentation of the functions for panres genes

--- code/databases/panres.py
+++ code/databases/panres.py
@@ -1,11 +1,11 @@
-from tkinter import N
 import pandas as pd
 from owlready2 import *
 import subprocess
+from loguru import logger
 
 import sys
 sys.path.append('..')
-from functions import get_instance, clean_gene_name, get_or_create_instance, get_or_create_subclass
+from functions import get_instance, clean_gene_name, get_or_create_instance
 import re
 
 database2name = {
@@ -19,27 +19,38 @@ database2name = {
     'resfinder': 'ResFinder'
 }
 
-def add_panres_genes(file: str, onto: Ontology, logger):
-    '''
-    Loop through panres genes and add them
-    '''
+def add_panres_genes(file: str, onto: Ontology, logger = logger):
+    """Add PanRes genes to the ontology
 
-    # Load file
+    Parameters
+    ----------
+    file : str
+        File containing metadata for the genes included in the first version of PanRes
+    onto : Ontology
+        The Ontology to load the genes into
+    logger : _type_
+        Logger object for logging messages.
+    """
+
+    # Load the metadata file (.tsv or .csv)
+
+    # Clean up gene names and database names in the metadata
     panres_metadata = pd.read_csv(file, sep=',' if file.endswith('.csv') else '\t', skiprows=1)
     panres_metadata['userGeneName'] = panres_metadata['userGeneName'].str.replace("_v1.0.0", "", regex=False)
     panres_metadata['chosenSeq'] = panres_metadata['chosenSeq'].str.replace("_v1.0.0", "", regex=False)
     panres_metadata['database'] = panres_metadata['database'].str.replace("_genes", "")
 
+    # Get a list of the unique gene names
     genes = panres_metadata['userGeneName'].unique().tolist()
 
-
+    # Loop through each unique gene, add it to the ontology and its annotations
     for gene in set(genes):
         new_gene = onto.PanGene(gene)
 
-        # Get annotation data
+        # Get annotation data for the correct gene
         m = panres_metadata.loc[panres_metadata['userGeneName'] == gene, ]
 
-        # Gene length
+        # add the gene length if its consistent across all entries
         if m['gene_len'].nunique() == 1:
             new_gene.has_length.append(int(m['gene_len'].values[0]))
         else:
@@ -47,22 +58,29 @@ def add_panres_genes(file: str, onto: Ontology, logger):
         
         # Loop through the annotation data for this gene
         for _, row in m.iterrows():
+            # Map the full database name from the short name
             database_shortname = row['database']
             database_name = database2name[database_shortname.lower()]
+            
+            # Get the instance and link the original gene name to the database
             database_instance = get_instance(onto, database_name)
             new_gene.is_from_database.append(database_instance)
 
+            # Clean and format the fasta header
             fasta_header = row['fa_header'].replace("~~~", "|").replace("'", "")
             gene_name = clean_gene_name(fasta_header, database_shortname.lower()).strip()
+
+            # Add the cleaned gene name as an individual of the class OriginalGene
             original_gene_instance = onto.OriginalGene(gene_name)
                         
+            # Track where the original gene name is from
             original_gene_instance.is_from_database.append(database_instance)
             original_gene_instance.original_fasta_header.append(fasta_header.strip() + '|' + database_name)
 
+            # Annotate that the pan gene name is the same as the original gene name
             new_gene.same_as.append(original_gene_instance)
-            # original_gene_instance.subclass_of.append(new_gene)
 
-            # gene cluster
+            # Get which cluster the gene belongs to 
             new_cluster = get_or_create_instance(onto = onto, cls = onto.PanGeneCluster, name=row['chosenSeq'].replace('pan', 'panc'))
             new_gene.member_of.append(new_cluster)
     
@@ -70,22 +88,37 @@ def add_panres_genes(file: str, onto: Ontology, logger):
     logger.success(f"Added PanRes genes (n={len(genes)}) to the ontology.")
 
 def add_panres_proteins(file: str, clstrs: str, onto: Ontology, logger):
+    """Adds PanRes proteins and their clusters to the ontology.
 
+    Parameters
+    ----------
+    file : str
+        Path to the faa file containing PanRes protein sequences.
+    clstrs : str
+        Path to the clstr file containing PanRes protein clusters, as determined by CD-HIT.
+    onto : Ontology
+        The ontology to load the protein information into
+    logger : loguru.logger
+        Logger object for logging messages
+    """
+
+    # Grep all headers of the protein sequences and extract them
     p = subprocess.run(f"grep '>' {file}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     protein_names = [l.split('_v1')[0].replace(">", "") for l in p.stdout.decode().split()]
 
-    gene_cluster_instances = onto.PanGeneCluster.instances()
+    # Loop through the protein names and add to the ontology
     for protein_name in protein_names:
         protein_instance = get_or_create_instance(onto = onto, cls = onto.PanProtein, name=protein_name.title())
+
+        # Get the corresponding gene cluster instance and link the protein name to it
         gene_cluster = get_instance(onto = onto, name = protein_name)
         if gene_cluster is not None:
             gene_cluster.translates_to.append(protein_instance)    
     
-        
-    
+    # Log the successfull addition  of proteins
     logger.success("Added PanRes proteins to the ontology.")
 
-    ## Read the CD-HIT output file
+    # Read the CD-HIT output file to extract cluster information
     p = re.compile(r">(pan\_\d+)")
     clusters = defaultdict(list)
     cluster_representative = None
@@ -94,22 +127,29 @@ def add_panres_proteins(file: str, clstrs: str, onto: Ontology, logger):
         for l in f.readlines():
             l = l.strip()
             m = p.findall(l)
+
+            # Check if the line indicates a new cluster
             if l.startswith('>Cluster'):
                 if cluster_representative is not None:
                     clusters[cluster_representative] = members
                     members, cluster_representative = [], None
+            # Else identify the cluster representative and it to the member list
             elif m and l.endswith('*'):
                 cluster_representative = m[0]
                 members.append(m[0])
+            # Add members to the current cluster
             else:
                 members.append(m[0])
 
+    # Loop through each cluster and add it to the ontology
     for cl, cl_members in clusters.items():
         cluster_protein_instance = get_or_create_instance(onto = onto, cls = onto.PanProteinCluster, name=cl.replace('pan', 'Pan'))
+
+        # Link each protein member to its cluster representative
         for cl_member in cl_members:
             cl_instance = get_or_create_instance(onto = onto, cls = onto.PanProtein, name = cl_member.title())
             if cl_instance is not None:
                 cl_instance.member_of.append(cluster_protein_instance)
         
-    
+    # Log the successful addition of protein clusters
     logger.success("Added PanRes protein clusters to the ontology.")
